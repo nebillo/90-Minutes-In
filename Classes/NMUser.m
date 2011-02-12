b1e91e78f374cdc30b80e0f944937dedd378c742
Nebil Kriedi <nkriedi@gmail.com>
2011-02-12 16:28:53 +0100

    NMUser implements the NSCoding protocol

--- Classes/NMUser.m
+++ Classes/NMUser.m
@@ -11,6 +11,29 @@
 
 @implementation NMUser
 
+#pragma mark -
+#pragma mark NSCoding
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+	[coder encodeObject:self.identifier forKey:@"identifier"];
+	[coder encodeObject:self.username forKey:@"username"];
+	[coder encodeObject:self.fullname forKey:@"fullname"];
+}
+
+
+- (id)initWithCoder:(NSCoder *)decoder {
+	if (self = [super init]) {
+		[self setIdentifier:[decoder decodeObjectForKey:@"identifier"]];
+		[self setUsername:[decoder decodeObjectForKey:@"username"]];
+		[self setFullname:[decoder decodeObjectForKey:@"fullname"]];
+	}
+	return self;
+}
+
+
+#pragma mark -
+#pragma mark Memory
+
 - (void)dealloc {
 	self.identifier = nil;
 	self.username = nil;
