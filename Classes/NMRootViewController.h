21937aabc5ee4eea74e6d78f24eb85483dd4b1ca
Nebil Kriedi <nkriedi@gmail.com>
2011-02-12 15:43:58 +0100

    creata struttura base progetto

---
+++ Classes/NMRootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  NinetyMinutes
+//
+//  Created by Nebil on 12/02/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMRootViewController : UIViewController {
+}
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@end
---
+++ Classes/NinetyMinutesAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  NinetyMinutesAppDelegate.h
+//  NinetyMinutes
+//
+//  Created by Nebil on 12/02/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NinetyMinutesAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
+
