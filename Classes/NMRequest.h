e7c9a3490f9d188be360603694414ae72160be8b
Nebil Kriedi <nkriedi@gmail.com>
2011-02-12 16:12:39 +0100

    created base class for http requests

---
+++ Classes/NMRequest.h
@@ -0,0 +1,58 @@
+//
+//  NMRequest.h
+//  NinetyMinutes
+//
+//  Created by Nebil Kriedi on 09/12/10.
+//  Copyright 2010 Nebil Kriedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class NMRequest;
+@protocol NMRequestDelegate <NSObject>
+
+- (void)request:(NMRequest *)request didFailWithError:(NSError *)error;
+- (void)request:(NMRequest *)request didFinishWithResponse:(id)response;
+
+@optional
+- (void)requestDidCancel:(NMRequest *)request;
+
+@end
+
+
+@class ASINetworkQueue;
+@class ASIHTTPRequest;
+@class GLUser;
+
+
+extern NSString * const kNMRequestDomain;
+
+
+@interface NMRequest : NSObject 
+{
+	id _delegate;
+	NSURL *_rootURL;
+	ASINetworkQueue *_networkQueue;
+	BOOL _cancelled;
+	id _userInfo;
+}
+
+- (id)initWithRootURL:(NSURL *)url;
+@property (nonatomic, readonly) NSURL *rootURL;
+
+@property(nonatomic, assign) id<NMRequestDelegate> delegate;
+@property (nonatomic, retain) id userInfo;
+
+@property (nonatomic, readonly, getter = isExecuting) BOOL executing;
+- (BOOL)start;
+- (void)cancel;
+
+// protected
+- (ASIHTTPRequest *)createMainRequest;
+- (id)createResponseForMainRequest:(ASIHTTPRequest *)request;
+- (NSError *)createErrorForResponseOfMainRequest:(ASIHTTPRequest *)request;
+
+- (void)mainRequestFinished:(ASIHTTPRequest *)request;
+- (void)mainRequestFailed:(ASIHTTPRequest *)request;
+
+@end
---
+++ Classes/NMRequest.m
@@ -0,0 +1,160 @@
+//
+//  NMRequest.m
+//  NinetyMinutes
+//
+//  Created by Nebil Kriedi on 09/12/10.
+//  Copyright 2010 Nebil Kriedi. All rights reserved.
+//
+
+#import "NMRequest.h"
+
+#import "ASIHTTPRequest.h"
+#import "ASINetworkQueue.h"
+
+
+NSString * const kNMRequestDomain = @"com.ninetyminutes.httpRequest";
+
+
+@implementation NMRequest
+
+- (id)initWithRootURL:(NSURL *)url {
+	if (self = [super init]) {
+		_rootURL = [url retain];
+		_networkQueue = [[ASINetworkQueue alloc] init];
+		[_networkQueue go];
+	}
+	return self;
+}
+
+
+- (void)dealloc {
+	[_rootURL release];
+	_cancelled = YES;
+	[_networkQueue cancelAllOperations];
+	[_networkQueue release];
+	[_userInfo release];
+	
+	[super dealloc];
+}
+
+
+@synthesize rootURL = _rootURL;
+@synthesize delegate = _delegate;
+@synthesize userInfo = _userInfo;
+
+
++ (NSMutableSet *)operationsPool {
+	static NSMutableSet *operationsPool = nil;
+	if (!operationsPool) {
+		operationsPool = [[NSMutableSet alloc] init];
+	}
+	return operationsPool;
+}
+
+
+- (BOOL)isExecuting {
+	return [_networkQueue operationCount] > 0;
+}
+			
+
+- (BOOL)start {
+	// cannot start while is still running
+	if ([self isExecuting]) {
+		return NO;
+	}
+	
+	ASIHTTPRequest *request = [self createMainRequest];
+	// invalid request
+	if (!request) {
+		return NO;
+	}
+	
+	_cancelled = NO;
+	[[[self class] operationsPool] addObject:self];
+	
+	[request setDelegate:self];
+	[request setDidFinishSelector:@selector(mainRequestFinished:)];
+	[request setDidFailSelector:@selector(mainRequestFailed:)];
+	[_networkQueue addOperation:request];
+	
+	return YES;
+}
+
+
+- (void)cancel {
+	_cancelled = YES;
+	[_networkQueue cancelAllOperations];
+	
+	if ([self.delegate respondsToSelector:@selector(requestDidCancel:)]) {
+		[self.delegate requestDidCancel:self];
+	}
+	
+	[[[self class] operationsPool] removeObject:self];
+}
+
+
+- (ASIHTTPRequest *)createMainRequest {
+	// overwrite in subclasses
+	return [ASIHTTPRequest requestWithURL:_rootURL];
+}
+
+
+- (id)createResponseForMainRequest:(ASIHTTPRequest *)request {
+	// overwrite in subclasses
+	return [request responseString];
+}
+
+
+- (NSError *)createErrorForResponseOfMainRequest:(ASIHTTPRequest *)request {
+	return [NSError errorWithDomain:kNMRequestDomain 
+							   code:[request responseStatusCode] 
+						   userInfo:request.userInfo];
+}
+
+
+- (void)mainRequestFinished:(ASIHTTPRequest *)request {
+	id response = [self createResponseForMainRequest:request];
+	if (response) {
+		NSLog(@"request finished with status: %d, %@: %@", 
+			  [request responseStatusCode], 
+			  [request requestMethod], 
+			  [request.url absoluteString]);
+		
+		[self.delegate request:self didFinishWithResponse:response];
+	} else {
+		NSLog(@"request response failure with status: %d, %@: %@", 
+			  [request responseStatusCode], 
+			  [request requestMethod], 
+			  [request.url absoluteString]);
+		
+		NSError *error = [self createErrorForResponseOfMainRequest:request];
+		[self.delegate request:self didFailWithError:error];
+	}
+	
+	[[[self class] operationsPool] removeObject:self];
+}
+
+
+- (void)mainRequestFailed:(ASIHTTPRequest *)request {
+	if (_cancelled) {
+		return;
+	}
+	
+	NSLog(@"request failed with error: %@, %@: %@", 
+		  [request error], 
+		  [request requestMethod], 
+		  [request.url absoluteString]);
+	
+	if ([request.error code] == ASIRequestCancelledErrorType) {
+		if ([self.delegate respondsToSelector:@selector(requestDidCancel:)]) {
+			[self.delegate requestDidCancel:self];
+		}
+	} else {
+		[self.delegate request:self didFailWithError:request.error];
+	}
+	
+	[[[self class] operationsPool] removeObject:self];
+}
+
+
+@end
