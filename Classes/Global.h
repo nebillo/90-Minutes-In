196c78f9a76b0773293193758b238665a7ba634d
Nebil Kriedi <nkriedi@gmail.com>
2011-02-12 17:40:22 +0100

    created global file and defined smoodit api params

---
+++ Classes/Global.h
@@ -0,0 +1,14 @@
+//
+//  Global.h
+//  NinetyMinutes
+//
+//  Created by Nebil on 12/02/11.
+//  Copyright 2011 Nebil Kriedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+extern NSString * const kAPIRootURL;
+extern NSString * const kAPIConsumerKey;
+extern NSString * const kAPIConsumerSecret;
---
+++ Classes/Global.m
@@ -0,0 +1,14 @@
+//
+//  Global.m
+//  NinetyMinutes
+//
+//  Created by Nebil on 12/02/11.
+//  Copyright 2011 Nebil Kriedi. All rights reserved.
+//
+
+#import "Global.h"
+
+
+NSString * const kAPIRootURL = @"http://smood.it"; 
+NSString * const kAPIConsumerKey = @"QPCG8XWtgbCf0hgrp0m8";
+NSString * const kAPIConsumerSecret = @"TmtYxixWvrvm9r9Y3h5Mh7nUjotmcM2PTrzFwgam";
