12a2599427e2a4fd0827664cd4748ce8431f36bc
Nebil Kriedi <nkriedi@gmail.com>
2011-02-17 21:51:27 +0100

    created class NMStatusUpdate

---
+++ Classes/NMStatusUpdate.h
@@ -0,0 +1,32 @@
+//
+//  NMStatusUpdate.h
+//  NinetyMinutes
+//
+//  Created by Nebil on 17/02/11.
+//  Copyright 2011 Nebil Kriedi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreLocation/CoreLocation.h>
+
+
+typedef enum {
+	NMStatusOut = 0,
+	NMStatusIn = 1,
+} NMStatus;
+
+
+@interface NMStatusUpdate : NSObject 
+{
+}
+
+- (id)initWithDictionary:(NSDictionary *)dict;
+
+@property (nonatomic, copy) NSString *identifier;
+@property (nonatomic, assign) NMStatus status;
+@property (nonatomic, retain) NSDate *createdAt;
+@property (nonatomic, retain) NSDate *expirationDate;
+@property (nonatomic, retain) CLLocation *location;
+@property (nonatomic, copy) NSString *address;
+
+@end
---
+++ Classes/NMStatusUpdate.m
@@ -0,0 +1,55 @@
+//
+//  NMStatusUpdate.m
+//  NinetyMinutes
+//
+//  Created by Nebil on 17/02/11.
+//  Copyright 2011 Nebil Kriedi. All rights reserved.
+//
+
+#import "NMStatusUpdate.h"
+
+
+@implementation NMStatusUpdate
+
+- (id)initWithDictionary:(NSDictionary *)dict {
+	if (self = [super init]) {
+		self.identifier = [dict objectForKey:@"id"];
+		
+		NSString *status = [dict objectForKey:@"status"];
+		if ([status isEqualToString:@"in"]) {
+			self.status = NMStatusIn;
+		} else {
+			self.status = NMStatusOut;
+		}
+		
+		NSString *location = [dict objectForKey:@"location"];
+		if (location) {
+			NSArray *coords = [location componentsSeparatedByString:@","];
+			CLLocation *location = [[[CLLocation alloc] initWithLatitude:[[coords objectAtIndex:0] floatValue] 
+															   longitude:[[coords objectAtIndex:1] floatValue]] autorelease];
+			self.location = location;
+			self.address = [dict objectForKey:@"address"];
+		}
+	}
+	return self;
+}
+
+
+- (void)dealloc {
+	self.identifier = nil;
+	self.createdAt = nil;
+	self.expirationDate = nil;
+	self.location = nil;
+	self.address = nil;
+	[super dealloc];
+}
+
+
+@synthesize identifier;
+@synthesize status;
+@synthesize createdAt;
+@synthesize expirationDate;
+@synthesize location;
+@synthesize address;
+
+@end
