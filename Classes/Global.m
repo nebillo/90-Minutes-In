b4d9819cee614fa51385eb1ae810bd4e432af819
Nebil Kriedi <nkriedi@gmail.com>
2011-02-20 13:28:42 +0100

    compiler flag for api url

--- Classes/Global.m
+++ Classes/Global.m
@@ -8,6 +8,12 @@
 
 #import "Global.h"
 
+//#define LOCALHOST 1
 
+#ifdef LOCALHOST
+NSString * const kAPIRootURL = @"http://localhost:8082/api";
+#else
 NSString * const kAPIRootURL = @"http://90minutesin.appspot.com/api";
+#endif
+
 NSString * const kFacebookAppId = @"152318558158118";
