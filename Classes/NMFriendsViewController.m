4efba81d2d42a654185608a23bf64951c8334127
Nebil Kriedi <nkriedi@gmail.com>
2011-03-01 23:33:41 +0100

    fixed memory issue on friends controller

--- Classes/NMFriendsViewController.m
+++ Classes/NMFriendsViewController.m
@@ -24,7 +24,7 @@
 - (id)init {
     if ((self = [super initWithNibName:@"NMFriendsViewController" bundle:nil])) {
         // Custom initialization
-		_user = [[NMAuthenticationManager sharedManager] authenticatedUser];
+		_user = [[[NMAuthenticationManager sharedManager] authenticatedUser] retain];
 		_friendsFilter = 0;
     }
     return self;
